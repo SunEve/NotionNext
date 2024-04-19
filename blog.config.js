aa693840eb5a2ba600683fd42bc3369b50890b67
HaoZhang <1315649364@qq.com>
2024-04-19 11:22:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -23,8 +23,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '1笑傲江湖', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '1室雅何须大，花香不在多...', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ZhangH', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '室雅何须大，花香不在多...', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.iitzh.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客', // 网站关键词 英文逗号隔开
 
