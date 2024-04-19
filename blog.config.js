5f858fd2aba1a8ea4fdeec5538b557ba176a405f
HaoZhang <1315649364@qq.com>
2024-04-19 10:15:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '笑傲江湖', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '室雅何须大，花香不在多...', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.iitzh.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '1315649364@qq.com', // 邮箱地址 例如mail@tangly1024.com
